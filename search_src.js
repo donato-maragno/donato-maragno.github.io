09799736ea6fc6361fcdd38958b6460daf81d2b9
dmaragno <donato.maragno1996@gmail.com>
2026-04-26 17:46:18 +0200

    Cleanup optimization scripts

--- search_src.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('./assets/index-YRROGjnA.js', 'utf8');
-
-let re = /src:\s*["']\/videos\//g;
-let match;
-while ((match = re.exec(content)) !== null) {
-    console.log('--- FOUND ---');
-    console.log(content.substring(match.index - 100, match.index + 200));
-}
