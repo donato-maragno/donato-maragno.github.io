5af81a8821e495499330f38723b877e43a55aab4
dmaragno <donato.maragno1996@gmail.com>
2026-04-26 14:28:21 +0200

    Cleanup debug scripts

--- inspect_zw.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open('./assets/index-YRROGjnA.js', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-idx = content.find("zw=()=>")
-if idx >= 0:
-    print("FOOTER:")
-    print(content[idx:idx+1500])
